2dc6cc48a83b1b9d1c77a07e4bf512157a967f6a
Ian Connor <iconnor@projectlounge.com>
2023-04-26 11:18:06 +1000

    Add github action workflow (#3)
    * Add workflow
    * Add customized library

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
 #include <Arduino.h>
+// Import libraries (BLEPeripheral depends on SPI)
+#include <SPI.h>
+#include <BLEPeripheral.h>
+
 #include <Wire.h>
 #include <MPU6050.h>
-#include <BLEPeripheral.h>
 
 // MPU-6050 Setup
 MPU6050 mpu;
@@ -11,12 +14,14 @@ BLEPeripheral blePeripheral = BLEPeripheral();
 BLEService sensorService = BLEService("1101"); // Custom service UUID
 BLECharacteristic sensorCharacteristic = BLECharacteristic("2101", BLENotify, 12); // Custom characteristic UUID, 12 bytes for X, Y, Z, and Resultant g-force values (3 bytes each)
 
+// Change the following line to use the other variant in .pio/libdeps/nrf52_dk/MPU6050/src/MPU6050.cpp
+// See https://github.com/jrowberg/i2cdevlib/issues/457
+#define dtostrf(Variable,Spaces,Precision ,S) (Variable)
+
 void setup() {
   // MPU-6050 Setup
   Wire.begin();
-  mpu.begin(MPU6050_SCALE_2000DPS, MPU6050_RANGE_2G);
-  mpu.setDLPFMode(MPU6050_DLPF_5HZ);
-  mpu.setSampleRateDivider(19);
+  mpu.initialize();
 
   // BLE Setup
   blePeripheral.setLocalName("HeadImpactDetector");
@@ -28,22 +33,11 @@ void setup() {
 
 void loop() {
   // Read MPU-6050 data
-  float ax, ay, az, gx, gy, gz;
+  int16_t ax, ay, az, gx, gy, gz;
   mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
 
-  // Calculate g-force values
-  float ax_g = ax / 16384.0 * 9.81;
-  float ay_g = ay / 16384.0 * 9.81;
-  float az_g = az / 16384.0 * 9.81;
-  float resultant_g = sqrt(ax_g * ax_g + ay_g * ay_g + az_g * az_g);
-
   // Prepare data for BLE transmission
-  int16_t bleData[4] = {
-    static_cast<int16_t>(ax_g * 100),
-    static_cast<int16_t>(ay_g * 100),
-    static_cast<int16_t>(az_g * 100),
-    static_cast<int16_t>(resultant_g * 100)
-  };
+  int16_t bleData[6] = { ax, ay, az, gx, gy, gz };
 
   // Update BLE characteristic with the new data
   sensorCharacteristic.setValue((unsigned char*)bleData, sizeof(bleData));
